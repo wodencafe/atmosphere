bd49f0ac17efc235f4deb5ef2c858864edc26e4b
Artur Signell <artur@vaadin.com>
2013-06-11 13:56:03 +0300

    Do not try to add a listener as it is always to late to add a listener at this point (#11898)
    Conflicts:
    	modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
    
    Change-Id: Ie0fb499305f34fc1ced177b6e29554661ee544de

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -1102,15 +1102,16 @@ public class AtmosphereFramework {
         s = sc.getInitParameter(PROPERTY_SESSION_SUPPORT);
         if (s != null) {
             config.setSupportSession(Boolean.valueOf(s));
-            if (sc.getServletContext().getMajorVersion() >= 3) {
-                try {
-                    sc.getServletContext().addListener(SessionSupport.class);
-                } catch (Throwable t) {
-                    logger.warn("SessionSupport error. Make sure you define {} as a listener in web.xml instead", SessionSupport.class.getName(), t);
-                }
-            } else {
-                logger.debug("Make sure you define {} as a listener in web.xml", SessionSupport.class.getName());
-            }
+            // if (sc.getServletContext().getMajorVersion() >= 3) {
+            // try {
+            // sc.getServletContext().addListener(SessionSupport.class);
+            // } catch (Throwable t) {
+            // logger.warn("SessionSupport error. Make sure you define {} as a listener in web.xml instead", SessionSupport.class.getName(), t);
+            // }
+            // } else {
+            // logger.debug("Make sure you define {} as a listener in web.xml",
+            // SessionSupport.class.getName());
+            // }
             isSessionSupportSpecified = true;
         }
         s = sc.getInitParameter(PROPERTY_ALLOW_SESSION_TIMEOUT_REMOVAL);
