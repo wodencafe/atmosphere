cf989fbedc018216c4acddd9d5758a96172f726b
Artur Signell <artur@vaadin.com>
2013-06-11 13:56:03 +0300

    Do not try to add a listener as it is always to late to add a listener at this point (#11898)
    Change-Id: I30986e1bb9fdd71ee20404247bbdb55625b31d50

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -831,16 +831,18 @@ public class AtmosphereFramework implements ServletContextProvider {
         s = sc.getInitParameter(PROPERTY_SESSION_SUPPORT);
         if (s != null) {
             config.setSupportSession(Boolean.valueOf(s));
-            if (sc.getServletContext().getMajorVersion() >= 3) {
-                try {
-                    sc.getServletContext().addListener(SessionSupport.class);
-                } catch (Throwable t) {
-                    logger.error("SessionSupport error", t);
-                    logger.debug("Make sure you define {} as a listener in web.xml", SessionSupport.class.getName());
-                }
-            } else {
-                logger.debug("Make sure you define {} as a listener in web.xml", SessionSupport.class.getName());
-            }
+            // if (sc.getServletContext().getMajorVersion() >= 3) {
+            // try {
+            // sc.getServletContext().addListener(SessionSupport.class);
+            // } catch (Throwable t) {
+            // logger.error("SessionSupport error", t);
+            // logger.debug("Make sure you define {} as a listener in web.xml",
+            // SessionSupport.class.getName());
+            // }
+            // } else {
+            // logger.debug("Make sure you define {} as a listener in web.xml",
+            // SessionSupport.class.getName());
+            // }
             isSessionSupportSpecified = true;
         }
         s = sc.getInitParameter(DISABLE_ONSTATE_EVENT);
