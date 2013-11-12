04383b24032c188f728eccf78a407aa50346f480
Artur Signell <artur@vaadin.com>
2013-11-12 15:30:50 +0200

    Removed commercial to avoid confusing users
    Change-Id: Ic035903b2be0413dead5d2b89092765f58c0aa02

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -993,9 +993,6 @@ public class AtmosphereFramework {
         logger.info("Atmosphere is using async support: {} running under container: {}",
                 getAsyncSupport().getClass().getName(), asyncSupport.getContainerName());
         logger.info("Atmosphere Framework {} started.", Version.getRawVersion());
-
-        logger.info("\n\n\tFor Atmosphere Framework Commercial Support, visit \n\t{} " +
-                "or send an email to {}\n", "http://www.async-io.org/", "support@async-io.org");
     }
 
     private void configureAnnotationPackages() {
